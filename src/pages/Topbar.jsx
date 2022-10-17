b00385e60b1fcbccd0bbea11b49caeea25f53b39
Samandar0021305 <TSamandardev@gmail.com>
2022-10-17 17:10:36 +0500

    init

--- src/pages/Topbar.jsx
+++ src/pages/Topbar.jsx
@@ -1,8 +1,26 @@
+import "./Topbar.scss";
+
+// img
+import navIcon from "../assets/icon/navbar-icon/navbar-icon.png";
 
 function Topbar(){
+    
+    const imgStyle = {
+        cursor:'pointer',
+        display:'flex'
+    }
+         
+
     return(
         <nav  className="navbar">
+          <div className="container">
+              <ul className="navbar__list">
+                <li>
+                   <img  style={imgStyle} src={navIcon} alt="" />
+                </li>
+              </ul>
 
+          </div>
         </nav>
     )
 }
