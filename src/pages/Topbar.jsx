f4b83ec7274290fc5ec0a7ece5990ef14a3cf3eb
Samandar0021305 <TSamandardev@gmail.com>
2022-10-17 17:17:57 +0500

    init

--- src/pages/Topbar.jsx
+++ src/pages/Topbar.jsx
@@ -2,7 +2,7 @@ import "./Topbar.scss";
 
 // img
 import navIcon from "../assets/icon/navbar-icon/navbar-icon.png";
-
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 function Topbar(){
     
     const imgStyle = {
@@ -17,6 +17,18 @@ function Topbar(){
               <ul className="navbar__list">
                 <li>
                    <img  style={imgStyle} src={navIcon} alt="" />
+                </li>
+                <li>
+                    <LocationOnIcon  style={{
+                        color:"white",
+                        width:"30px",
+                        height:"30px",
+                        cursor:'pointer',
+                       display:'flex'
+                    }}  />
+                </li>
+                <li>
+                    <a href="tel:8 981 120-11-17">8 981 120-11-17</a>
                 </li>
               </ul>
 
