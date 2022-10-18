4351115181b65e5addd237ecc11c65564d64c81b
Samandar0021305 <TSamandardev@gmail.com>
2022-10-18 21:43:37 +0500

    init

--- src/components/MainPageComponents/MainPageHeader/MainPageHeader.jsx
+++ src/components/MainPageComponents/MainPageHeader/MainPageHeader.jsx
@@ -6,8 +6,10 @@ function MainPageHeader(){
     return(
         <header className="header">
             <Topbar />
-            <div className="header__box">
-               
+            <div className="header__box container">
+                <h2>Исключительное качество без компромиссов</h2>
+                <p>Ножи «Tuotown» – это главный инструмент поваров и секрет кулинарного мастерства</p>
+                <button>ПОДРОБНЕЕ</button>
             </div>
         </header>
     )
