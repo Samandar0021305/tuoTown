2b4ea678afc06deb1f015b5da82453e175ed0736
Samandar0021305 <TSamandardev@gmail.com>
2022-10-19 16:36:48 +0500

    init

--- src/components/MainPageComponents/MainPageSection/sectionTwo/SectionTwo.jsx
+++ src/components/MainPageComponents/MainPageSection/sectionTwo/SectionTwo.jsx
@@ -1,11 +1,12 @@
 import "./sectionTwo.scss";
+import  imgBack from "../../../../assets/img/section/2-blok/Rectangle 33.png";
 
 function SectionTwo({restApi}){
     console.log(restApi);
     return(
         <section className="sectionTwo">
            <span className="sectionTwo__bg">
-                
+                <img src={imgBack} alt="" />
            </span>
         </section>
     )
