b9bf015777abcb8063fa18f2aab0c3138a3e5c63
Samandar0021305 <TSamandardev@gmail.com>
2022-10-20 14:29:51 +0500

    init

--- src/components/MainPageComponents/MainPageSection/sectionFor/SectionFor.jsx
+++ src/components/MainPageComponents/MainPageSection/sectionFor/SectionFor.jsx
@@ -1,15 +1,58 @@
 import "./SectionFor.scss"
-
+import "../sectionTwo/sectionTwo.scss";
+import AddIcon from '@mui/icons-material/Add';
+// import "./sectionTree.scss"
+import card3 from "../../../../assets/img/section/2-blok/card3.png"
+import card4 from "../../../../assets/img/section/2-blok/card4.png"
+import card5 from "../../../../assets/img/section/2-blok/card1.png"
+import card6 from "../../../../assets/img/section/2-blok/card6.png"
 function SectionFor(){
     return(
         <div className="sectionFor">
            <div>
            <h2 className="sectionFor__title">Узнавайте первым о новинках и новостях</h2>
-            <input  className="sectionFor__input"  type="text" />
+            <input  className="sectionFor__input"  placeholder="Ваш e-mail" type="text" />
            </div>
-           <ul className="sectionFor__list">
-               
-           </ul>
+           <ul className="sectionTwo__list">
+            <li className="SectionTree__item">
+            
+                 <span>
+                 <AddIcon  className='AddIcon'/>
+                    <img src={card3} alt="" />
+                 </span>
+                 <h3>Складной нож SQ01-B</h3>
+                 <p>850 р.</p>
+            </li>
+            <li className="SectionTree__item">
+            
+                 <span>
+                 <AddIcon  className='AddIcon'/>
+                    <img src={card4} alt="" />
+                 </span>
+                 <h3>Складной нож SQ01-B</h3>
+                 <p>850 р.</p>
+            </li>
+            <li className="SectionTree__item1">
+            
+            <span>
+                 <AddIcon  className='AddIcon'/>
+                    <img src={card5} alt="" />
+                 </span>
+                 <h3>Складной нож SQ01-B</h3>
+                 <p>850 р.</p>
+         
+
+            </li>
+            <li className="SectionTree__item">
+            
+                 <span>
+                 <AddIcon  className='AddIcon'/>
+                    <img src={card6} alt="" />
+                 </span>
+                 <h3>Складной нож SQ01-B</h3>
+                 <p>850 р.</p>
+            </li>
+        </ul>
         </div>
     )
 }
