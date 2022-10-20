d843a095a228d0f6b2b8a200b520f86f0afe6ab6
Samandar0021305 <TSamandardev@gmail.com>
2022-10-20 14:21:08 +0500

    init

--- src/components/MainPageComponents/MainPageSection/sectionFor/SectionFor.jsx
+++ src/components/MainPageComponents/MainPageSection/sectionFor/SectionFor.jsx
@@ -2,8 +2,14 @@ import "./SectionFor.scss"
 
 function SectionFor(){
     return(
-        <div>
-            
+        <div className="sectionFor">
+           <div>
+           <h2 className="sectionFor__title">Узнавайте первым о новинках и новостях</h2>
+            <input  className="sectionFor__input"  type="text" />
+           </div>
+           <ul className="sectionFor__list">
+               
+           </ul>
         </div>
     )
 }
